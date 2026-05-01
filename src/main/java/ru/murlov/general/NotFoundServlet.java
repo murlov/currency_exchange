43e8ccaa620ad31f321d4449363b688e5078cc91
murlov <amolovv@gmail.com>
2026-05-01 21:26:37 +0300

    feat: add global servlet for handling 404

---
+++ src/main/java/ru/murlov/general/NotFoundServlet.java
@@ -0,0 +1,27 @@
+package ru.murlov.general;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import jakarta.servlet.*;
+import jakarta.servlet.http.*;
+import jakarta.servlet.annotation.*;
+
+import java.io.IOException;
+import java.util.Map;
+
+@WebServlet("/*")
+public class NotFoundServlet extends HttpServlet {
+    @Override
+    protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        response.setContentType("application/json");
+        response.setStatus(404);
+        ObjectMapper mapper = new ObjectMapper();
+
+        Map<String, Object> error = Map.of(
+                "status", 404,
+                "error", "Not found",
+                "message", "Endpoint not found"
+        );
+
+        mapper.writeValue(response.getWriter(), error);
+    }
+}
