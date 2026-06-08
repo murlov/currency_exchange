7a538baf08b31b7c4dc9b982663e9273b0c39b39
murlov <amolovv@gmail.com>
2026-06-08 18:33:36 +0300

    feat: add ExchangeRate model

---
+++ src/main/java/ru/murlov/model/ExchangeRate.java
@@ -0,0 +1,49 @@
+package ru.murlov.model;
+
+public class ExchangeRate {
+
+    private Long id;
+    private Currency base_currency;
+    private Currency target_currency;
+    private float rate;
+
+    public ExchangeRate(Long id, Currency base_currency, Currency target_currency, float rate) {
+        this.id = id;
+        this.base_currency = base_currency;
+        this.target_currency = target_currency;
+        this.rate = rate;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Currency getBase_currency() {
+        return base_currency;
+    }
+
+    public void setBase_currency(Currency base_currency) {
+        this.base_currency = base_currency;
+    }
+
+    public Currency getTarget_currency() {
+        return target_currency;
+    }
+
+    public void setTarget_currency(Currency target_currency) {
+        this.target_currency = target_currency;
+    }
+
+    public float getRate() {
+        return rate;
+    }
+
+    public void setRate(float rate) {
+        this.rate = rate;
+    }
+
+}
