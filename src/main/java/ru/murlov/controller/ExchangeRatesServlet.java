149015f1f7fea4a6da99275b414defb0464881a4
murlov <amolovv@gmail.com>
2026-06-09 11:58:14 +0300

    feat: add ExchangeRatesServlet

---
+++ src/main/java/ru/murlov/controller/ExchangeRatesServlet.java
@@ -0,0 +1,33 @@
+package ru.murlov.controller;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import jakarta.servlet.*;
+import jakarta.servlet.http.*;
+import jakarta.servlet.annotation.*;
+import ru.murlov.dto.ExchangeRateDto;
+import ru.murlov.service.ExchangeRateService;
+
+import java.io.IOException;
+import java.util.List;
+
+@WebServlet("/exchangeRates")
+public class ExchangeRatesServlet extends BaseServlet {
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        response.setContentType("application/json");
+        ExchangeRateService exchangeRateService = new ExchangeRateService();
+        ObjectMapper mapper = new ObjectMapper();
+        List<ExchangeRateDto> exchangeRateDtos = exchangeRateService.getAll();
+
+        sendResponse(response, 200, exchangeRateDtos, mapper);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+    }
+
+    private void sendResponse(HttpServletResponse response, int status, Object value, ObjectMapper mapper) throws IOException {
+        response.setStatus(status);
+        mapper.writeValue(response.getWriter(), value);
+    }
+}
