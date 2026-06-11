fcba9fdcbfe7cc3983968bdafb275dbc5ca2ab34
murlov <amolovv@gmail.com>
2026-06-11 03:34:29 +0300

    feat: add ExchangeRateServlet

---
+++ src/main/java/ru/murlov/controller/ExchangeRateServlet.java
@@ -0,0 +1,55 @@
+package ru.murlov.controller;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import jakarta.servlet.*;
+import jakarta.servlet.http.*;
+import jakarta.servlet.annotation.*;
+import ru.murlov.dto.ExchangeRateDto;
+import ru.murlov.exception.NotFoundException;
+import ru.murlov.exception.ValidationException;
+import ru.murlov.service.ExchangeRateService;
+
+import java.io.IOException;
+
+@WebServlet("/exchangeRate/*")
+public class ExchangeRateServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        response.setContentType("application/json");
+        ExchangeRateService exchangeRateService = new ExchangeRateService();
+        ObjectMapper mapper = new ObjectMapper();
+
+        String pathInfo = request.getPathInfo();
+
+
+        if (pathInfo == null || pathInfo.isEmpty() || pathInfo.equals("/")) {
+            throw new ValidationException("Missing currency codes pair");
+        }
+
+        String[] parts = pathInfo.split("/");
+        if (parts.length != 2) {
+            throw new NotFoundException("Invalid path");
+        }
+
+        String codesPair = parts[1];
+        if (!codesPair.matches("[A-Z]{6}")) {
+            throw new ValidationException("Currency codes pair must contain exactly 6 letters");
+        }
+
+        String baseCurrencyCode = codesPair.substring(0,3);
+        String targetCurrencyCode = codesPair.substring(3, 6);
+
+        ExchangeRateDto exchangeRateDto = exchangeRateService.getByCodesPair(baseCurrencyCode, targetCurrencyCode);
+        sendResponse(response, 200, exchangeRateDto, mapper);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+
+    private void sendResponse(HttpServletResponse response, int status, Object value, ObjectMapper mapper) throws IOException {
+        response.setStatus(status);
+        mapper.writeValue(response.getWriter(), value);
+    }
+}
