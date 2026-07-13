f6586338d13cc6c376e0e37b631effcca1ff68a8
murlov <amolovv@gmail.com>
2026-07-13 07:28:49 +0300

    refactor: remove redundant method toModel() from ExchangeRateMapper

--- src/main/java/ru/murlov/mapper/ExchangeRateMapper.java
+++ src/main/java/ru/murlov/mapper/ExchangeRateMapper.java
@@ -5,22 +5,12 @@ import ru.murlov.model.ExchangeRate;
 
 public class ExchangeRateMapper {
 
-    public static ExchangeRateDto toDto(ExchangeRate exchangeRate) {
-        return new ExchangeRateDto(
     public static ExchangeRateResponse toDto(ExchangeRate exchangeRate) {
         return new ExchangeRateResponse(
                 exchangeRate.getId(),
                 CurrencyMapper.toDto(exchangeRate.getBase_currency()),
                 CurrencyMapper.toDto(exchangeRate.getTarget_currency()),
                 exchangeRate.getRate()
-        );
-    }
-
-    public static ExchangeRate toModel(ExchangeRateDto exchangeRateDto) {
-        return new ExchangeRate(
-                exchangeRateDto.base_currency(),
-                exchangeRateDto.target_currency(),
-                exchangeRateDto.rate()
         );
     }
 }
