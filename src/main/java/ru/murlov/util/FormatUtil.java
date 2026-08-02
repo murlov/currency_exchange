238fd1b475c62bd367a6a61dbbc2ff35b25d8b7b
murlov <amolovv@gmail.com>
2026-08-02 18:28:22 +0300

    feat: add float rounding to FormatUtil

--- src/main/java/ru/murlov/util/FormatUtil.java
+++ src/main/java/ru/murlov/util/FormatUtil.java
@@ -8,24 +8,26 @@ public final class FormatUtil {
     private FormatUtil() {}
 
     public static String getRequiredNormalizedStringParameter(HttpServletRequest request, String parameterName) {
-        String value = request.getParameter(parameterName);
-        if (value == null || value.isBlank()) {
+        String text = request.getParameter(parameterName);
+        if (text == null || text.isBlank()) {
             throw new ValidationException("Parameter '" + parameterName + "' is required");
         }
 
-        return value.strip();
+        return text.strip();
     }
 
     public static float getRequiredNormalizedFloatParameter(HttpServletRequest request, String parameterName) {
-        String value = request.getParameter(parameterName);
-        if (value == null || value.isBlank()) {
+        String text = request.getParameter(parameterName);
+        if (text == null || text.isBlank()) {
             throw new ValidationException("Parameter '" + parameterName + "' is required");
         }
 
         String capitalizedParameterName = parameterName.substring(0,1).toUpperCase() + parameterName.substring(1);
+        float value = Float.parseFloat(text.strip());
+        float result = (float) Math.round(value * 100) / 100f;
 
         try {
-            return Float.parseFloat(value.strip());
+            return result;
         } catch (NumberFormatException e) {
             throw new ValidationException(capitalizedParameterName + " must be a decimal number.");
         }
