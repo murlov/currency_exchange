7661afd50092f912797dd9534b668e9fd0d6c29f
murlov <amolovv@gmail.com>
2026-08-09 19:59:43 +0300

    refactor: remove redundant setters and make fields final

--- src/main/java/ru/murlov/model/Currency.java
+++ src/main/java/ru/murlov/model/Currency.java
@@ -3,9 +3,9 @@ package ru.murlov.model;
 public class Currency {
 
     private int id;
-    private String code;
-    private String name;
-    private String sign;
+    private final String code;
+    private final String name;
+    private final String sign;
 
     public Currency(int id, String code, String name, String sign) {
         this.id = id;
@@ -32,24 +32,12 @@ public class Currency {
         return code;
     }
 
-    public void setCode(String code) {
-        this.code = code;
-    }
-
     public String getName() {
         return name;
     }
 
-    public void setName(String name) {
-        this.name = name;
-    }
-
     public String getSign() {
         return sign;
-    }
-
-    public void setSign(String sign) {
-        this.sign = sign;
     }
 
     @Override
--- src/main/java/ru/murlov/model/ExchangeRate.java
+++ src/main/java/ru/murlov/model/ExchangeRate.java
@@ -5,9 +5,9 @@ import java.math.BigDecimal;
 public class ExchangeRate {
 
     private int id;
-    private Currency base_currency;
-    private Currency target_currency;
-    private BigDecimal rate;
+    private final Currency base_currency;
+    private final Currency target_currency;
+    private final BigDecimal rate;
 
     public ExchangeRate(int id, Currency base_currency, Currency target_currency, BigDecimal rate) {
         this.id = id;
@@ -34,24 +34,11 @@ public class ExchangeRate {
         return base_currency;
     }
 
-    public void setBase_currency(Currency base_currency) {
-        this.base_currency = base_currency;
-    }
-
     public Currency getTarget_currency() {
         return target_currency;
-    }
-
-    public void setTarget_currency(Currency target_currency) {
-        this.target_currency = target_currency;
     }
 
     public BigDecimal getRate() {
         return rate;
     }
-
-    public void setRate(BigDecimal rate) {
-        this.rate = rate;
-    }
-
 }
